0bcbc94e718a44a63e2c4dacd4e9c148dd6d0be9
Igor Santos <igorasantos@gmail.com>
2019-03-23 07:16:55 -0300

    DW1 A11 cont

--- 3/DW1/courseware/A11/WebContent/p04/q3.js
+++ 3/DW1/courseware/A11/WebContent/p04/q3.js
@@ -1,74 +1,123 @@
-// Altere o exemplo anterior para criar uma funcionalidade de EDITAR:
-// ao se clicar no nome de uma cidade listada na tabela, os dados dela serão levados ao formulário para edição por parte do usuário.
-// Ao se clicar no botão do formulário, as alterações feitas pelo usuário devem ser levadas de volta para a tabela, na linha que estava sendo editada. Dica: use o vetor rows para acessar as linhas já existentes.
+//still not working
+
 var formEditingRow = false;
-function inserirRegistro(){
-	//get table body
-	var tableBody = document.getElementById('registros');
-	//get form fields
-	var city = document.getElementById('inputCity').value;
-	var pop = document.getElementById('inputCityPop').value;
-	var constState = document.getElementById('inputConstState').value;
-	var sovState = document.getElementById('inputSovState').value;
-	
-	if (formEditingRow == false){
 
+function main(l){
+	var index = readIndex(l);
+	if (formEditingRow == true){
+		dataRecToForm(index);
 	}
-
-	// Recupera a quantidade de linhas atuais
-	var totalLines = tableBody.rows.length;
-
-	//adiciona a nova linha no fim da tableBody (1a posição é 0)
-	var newLine = tableBody.insertRow(totalLines);
-
-	//adiciona celulas vazias à linha criada
-	var newCellCity=newLine.insertCell(0);
-	var newCellPop=newLine.insertCell(1);
-	var newCellConstState = newLine.insertCell(2);
-	var newCellSovState = newLine.insertCell(3);
-
-	//adiciona conteúdo nas células criadas
-	newCellCity.innerHTML=city+"  ";
-	newCellPop.innerHTML=pop;
-	newCellConstState.innerHTML = constState;
-	newCellSovState.innerHTML = sovState;
-
-	//cria link
-	var link = document.createElement("a");
-	link.href = "#";
-	var paramOnClick = `editarRegistro(${totalLines})`;
-	link.setAttribute("onclick",paramOnClick);
-
-	//adiciona ícone de edição	
-	var icon = document.createElement("i");
-	icon.setAttribute("class","fas fa-pen fa-sm");
-	
-	//adicionar ícone ao link
-	link.appendChild(icon);
-	
-	//adiciona link à célula
-	newCellCity.appendChild(link);
+	var newEmptyLine = createDataStruct();
+	var arrayInputs = new Array();
+	arrayInputs = readInputs();
+	eraseForm();
+	var newFilledLine = dataFilling(newEmptyLine, arrayInputs, index);
+	updateTable(newFilledLine, index);
+	gameOver();
 }
-function editarRegistro(l){
-	formEditingRow = true;
-	console.log(l);
-	var test = document.getElementById("registros").rows[l];
-	// console.log(test.rows[l].cells[1].innerHTML);
-	console.log(test);
 
+function startGame(i){
+	formEditingRow = true;
+	main(i);
+}
+
+function readIndex(lin){
+	var ind;
+	if (formEditingRow == true){
+		ind = lin;
+	} else {
+		ind = document.getElementById('registros').rows.length;
+	}
+	return ind;
+}
+
+function dataRecToForm(line){
+	//debug
+	console.log(line);
+	
+	//recupera TR
+	var test = document.getElementById("registros").rows[l];
+
+	//debug
+	console.log(test);
+	
+	//preenche form inputs
 	document.getElementById('inputCity').value = test.cells[0].innerText;
 	document.getElementById('inputCityPop').value = test.cells[1].innerText;
 	document.getElementById('inputConstState').value = test.cells[2].innerText;
 	document.getElementById('inputSovState').value = test.cells[3].innerText;
-	// inputCity
-	// inputCityPop
-	// inputConstState
-	// inputSovState
-	
 }
-function eraseForm(){
+
+function createDataStruct(){
+	//adiciona nova linha temporária, ainda sem vínculo
+	var newLine = document.createElement("tr");
+	//adiciona celulas vazias à linha temporária criada
+	// var c1 = newLine.insertCell(0);
+	// var c2 = newLine.insertCell(1);
+	// var c3 = newLine.insertCell(2);
+	// var c4 = newLine.insertCell(3);
+	newLine.insertCell(0); // aplicar um for aqui depois
+	newLine.insertCell(1);
+	newLine.insertCell(2);
+	newLine.insertCell(3);
+	return newLine;
+}
+
+function readInputs() {
+	var i1 = document.getElementById('inputCity').value;
+	var i2 = document.getElementById('inputCityPop').value;
+	var i3 = document.getElementById('inputConstState').value;
+	var i4 = document.getElementById('inputSovState').value;
+	var arrayTemp = [i1, i2, i3, i4];
+	return arrayTemp;
+}
+
+function eraseForm() {
 	document.getElementById('inputCity').value = "";
 	document.getElementById('inputCityPop').value = null;
 	document.getElementById('inputConstState').value = "";
 	document.getElementById('inputSovState').value = "";
+}
+
+function dataFilling(nEmpL, aInp, indx){
+	//aplicar um for aqui depois
+	// ou seria innerHTML?
+	nEmpL.cells[0].innerText = aInp[0];
+	nEmpL.cells[1].innerText = aInp[1];
+	nEmpL.cells[2].innerText = aInp[2];
+	nEmpL.cells[3].innerText = aInp[3];
+
+	// 1a celula - cria link
+	var link = document.createElement("a");
+	link.href = "#";
+	// var paramOnClick = `main(${indx})`;
+	link.setAttribute("onclick", `startGame(${indx})`);
+	link.innerText = " ";
+
+	// 1a celula - adiciona ícone de edição	
+	var icon = document.createElement("i");
+	icon.setAttribute("class", "fas fa-pen fa-sm");
+
+	//1a celula - adicionar ícone ao link
+	link.appendChild(icon);
+
+	// 1a celula - adiciona link à célula
+	nEmpL.cells[0].appendChild(link);
+	return nEmpL;
+}
+
+function updateTable(nFillL, idx){
+	var tableBody = document.getElementById('registros');
+	if (formEditingRow == true) {
+		// debug
+		console.log(tableBody.rows[idx]);
+
+		tableBody.rows[idx] = nFillL;
+	} else {
+		tableBody.insertRow(nFillL);
+	}
+}
+
+function gameOver(){
+	formEditingRow = false;
 }
