a344e9e76415399a74a8545c444a246856cdfc8a
Igor Santos <igorasantos@gmail.com>
2019-05-06 10:48:08 -0300

    DW2 A02

---
+++ 3/DW2/courseware/A02/src/br/ufrn/imd/tec/dw2/a02/controllers/CadastroMBean.java
@@ -0,0 +1,22 @@
+package br.ufrn.imd.tec.dw2.a02.controllers;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import br.ufrn.imd.tec.dw2.a02.model.Pessoa;
+
+@ManagedBean
+@SessionScoped
+public class CadastroMBean {
+	Pessoa pessoa;
+	public CadastroMBean() {
+		pessoa = new Pessoa();
+	}
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}	
+	public String cadastrar() {
+		return "/sucesso.jsf";
+	}
+}
---
+++ 3/DW2/courseware/A02/src/br/ufrn/imd/tec/dw2/a02/model/Pessoa.java
@@ -0,0 +1,35 @@
+package br.ufrn.imd.tec.dw2.a02.model;
+
+import java.util.Date;
+
+public class Pessoa {
+	private String nome;
+	private String endereco;
+	private Date dataNascimento;
+	private String sexo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+}
