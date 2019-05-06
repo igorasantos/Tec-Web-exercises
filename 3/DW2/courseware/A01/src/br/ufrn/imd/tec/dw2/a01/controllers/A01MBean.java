ea2825bbdf99f847302cf087ca1dfe38e345ff5f
Igor Santos <igorasantos@gmail.com>
2019-05-06 08:14:02 -0300

    DW2 A01

---
+++ 3/DW2/courseware/A01/src/br/ufrn/imd/tec/dw2/a01/controllers/A01MBean.java
@@ -0,0 +1,8 @@
+package br.ufrn.imd.tec.dw2.a01.controllers;
+import javax.faces.bean.ManagedBean;
+@ManagedBean
+public class A01MBean {
+	public String getWorld() {
+		return "Hello World JSF! A01";
+	}
+}
