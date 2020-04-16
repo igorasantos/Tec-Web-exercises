a4af404fdb33e8e0b177f8fbe752b23d11683643
Igor Santos <igorasantos@gmail.com>
2020-04-16 18:05:52 -0300

    Form exercise update - js
    3 / DW1 / courseware / A11 / WebContent / p09 / q2.js

--- 3/DW1/courseware/A11/WebContent/p09/q2.js
+++ 3/DW1/courseware/A11/WebContent/p09/q2.js
@@ -1,47 +1,89 @@
 function printChar(txt, i) {
-  var cachedLetter = txt.charAt(i);
-  document.getElementById('divResult').classList.remove('d-none');
-  document.getElementById('divResult').classList.add('d-block');
-  document.getElementById('divResult').innerText = cachedLetter;
-}
-function validateInput2() {
-  var sizeInput1 = document.getElementById('formInput1').value.length;
-  var input2 = document.getElementById('formInput2');
-  if (sizeInput1 > input2.value) {
-    input2.setCustomValidity("");
+  let cachedLetter = txt.charAt(i);
+  let areaAlerta = document.getElementById('divResult');
+  areaAlerta.classList.remove('d-none');
+  areaAlerta.classList.add('d-block');
+  areaAlerta.innerText = cachedLetter;
+};
+
+function apagaAlert() {
+  let areaAlerta = document.getElementById('divResult');
+  if (!areaAlerta.classList.contains('d-none')) {
+    areaAlerta.innerText = '';
+    areaAlerta.classList.remove('d-block');
+    areaAlerta.classList.add('d-none');
+  }
+};
+
+function validateInputNumerico(inputTxt, inputNum) {
+  let sizeInputTxt = inputTxt.value.length;
+  let valorInputNum = inputNum.value;
+  if (sizeInputTxt > valorInputNum) {
+    inputNum.setCustomValidity('');
   } else {
-    input2.setCustomValidity("Campo invalido");
+    inputNum.setCustomValidity('Campo invalido');
+  }
+};
+
+function adicionaEstadoFormValidado(form) {
+  if (!form.classList.contains('was-validated')) {
+    form.classList.add('was-validated');
   }
 }
-// adapted from: https://getbootstrap.com/docs/4.3/components/forms/#custom-styles
+
+function removeEstadoFormValidado(form) {
+  if (form.classList.contains('was-validated')) {
+    form.classList.remove('was-validated');
+  }
+};
+
 (function () {
-  'use strict';
-  window.addEventListener('load', function () {
-    var forms = document.getElementsByClassName('needs-validation');
-    // it's just running one time - needs to be improved
-    var validation = Array.prototype.filter.call(forms, function (form) {
-      // pt: form é o formulário em questão, na lista de formulários existentes na página
-      form.addEventListener('submit', function (event) {
-        //adaptation starts here
-        validateInput2();
-        if (form['formInput2'].checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-          // kludge - start
-          document.getElementById('invalidMsg').style.display = 'block';
-          // kludge - end
-        } else if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        } /* just to see when the validation is true - start */ else {
-          event.preventDefault();
-          event.stopPropagation();
-          // just to see when the validation is true - end
-          printChar(form['formInput1'].value, form['formInput2'].value);
-        }
-        //adaptation ends here
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+  window.addEventListener(
+    'load',
+    function () {
+      let forms = document.getElementsByClassName('needs-validation');
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          'submit',
+          function (event) {
+            validateInputNumerico(form['formInput1'], form['formInput2']);
+            if (!form['formInput2'].checkValidity() || !form.checkValidity()) { // se o teste ou algum outro input no form tiver inválido
+              apagaAlert();
+              event.preventDefault();
+              event.stopPropagation();
+              adicionaEstadoFormValidado(form);
+            } else { // se tiver ok
+              event.preventDefault();
+              event.stopPropagation();
+              printChar(form['formInput1'].value, form['formInput2'].value); // comportamento adicional
+              adicionaEstadoFormValidado(form);
+            }
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+
+(function () {
+  window.addEventListener(
+    'load',
+    function () {
+      let forms = document.getElementsByClassName('needs-validation');
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          'reset',
+          function () {
+            removeEstadoFormValidado(form);
+            apagaAlert();
+          },
+          false
+        );
+      });
+    },
+    false
+  );
 })();
